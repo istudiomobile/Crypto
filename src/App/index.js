89a02485a20c10c996b48c5b30c2479da03f5f92
istudiomobile <mexicanchevys@hotmail.com>
2019-01-29 23:22:27 -0600

    Moved appbar components to new component.

--- src/App/index.js
+++ src/App/index.js
@@ -2,23 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import styled, {css} from 'styled-components';
 import AppLayout from "./AppLayout";
+import AppBar from './AppBar';
 
-const Logo = styled.div`
-  font-size: 1.5em;
-`
-
-const ControlButton = styled.div`
-cursor: pointer;
-  ${props => props.active && css`
-      text-shadow: 0px 0px 60px #03ff03
-  `};
-`
-
-const Bar = styled.div`
-  margin-bottom: 40px;
-  display: grid;
-  grid-template-columns: 180px auto 100px 100px;
-`
 
 const Content = styled.div`
 
@@ -69,18 +54,7 @@ class App extends Component {
   render() {
     return (
       <AppLayout>
-        <Bar>
-          <logo>
-            CryptoDash
-          </logo>
-          <div></div>
-          {!this.state.firstVisit && (<ControlButton onClick={() => {this.setState({page: 'dashboard'})}} active={this.displayingDashboard()}>
-            Dashboard
-          </ControlButton>)}
-          <ControlButton onClick={() => {this.setState({page: 'settings'})}} active={this.displayingSettings()}>
-            Settings
-          </ControlButton>
-        </Bar>
+        <AppBar></AppBar>
         <Content>
           {this.displayingSettings() && this.settingsContent()}
         </Content>
