794217ffcf5cfec9a0de56405b1b10abfc96460f
istudiomobile <mexicanchevys@hotmail.com>
2019-02-05 23:32:30 -0600

    Added CryptoCompare.

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const cc = require('cryptocompare');
+
 export const AppContext = React.createContext();
 
 export class AppProvider extends React.Component {
@@ -7,9 +9,41 @@ export class AppProvider extends React.Component {
         super(props);
         this.state = {
             page: 'dashboard',
-            setPage: this.setPage
+            ...this.savedSettings(),
+            setPage: this.setPage,
+            confirmFavorites: this.confirmFavorites
         }
     }
+
+    componentDidMount = () => {
+        this.fetchCoins();
+    };
+
+    fetchCoins = async () => {
+        let coinList = (await cc.coinList().Data);
+        this.setState({coinList});
+    };
+
+    confirmFavorites = () => {
+        this.setState({
+            firstVisit: false,
+            page: 'dashboard'
+        });
+        localStorage.setItem('cryptoDash', JSON.stringify(
+            {
+                test: 'hello!'
+            }
+        ))
+    }
+
+    savedSettings() {
+        let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+        if(!cryptoDashData) {
+            return {page: 'settings', firstVisit: true}
+        }
+        return {};
+    }
+
     setPage = page => this.setState({page});
     render() {
         const { children } = this.props;
